98c5962373dd72bba20d5d45c5eb4d8c34676c72
Zerodesignz <Japzdesignz@google.com>
2023-08-10 15:45:23 -0400

    change

--- src/components/InfoTabs/index.js
+++ src/components/InfoTabs/index.js
@@ -22,7 +22,7 @@ const InfoTab = () => {
               <Stuff>
               <span>
               <h1>Collect</h1> 
-              <img src="images/icons/collection.png" alt=" collection icon" />
+              <img src="images/icons/collection.png" alt="collection icon" />
               <text>
               <p>Algo Leagues is a collection of 19 unique character NFTs on the Algorand blockchain. Each character has its own backstory, traits, and statistics, making them a unique addition to your NFT collection.</p>
               </text>
@@ -41,7 +41,7 @@ const InfoTab = () => {
               <Stuff>
               <span>
               <h1>Stake</h1>
-              <img src="images/icons/stakeIcon.png" alt=" stake icon" />
+              <img src="images/icons/stakeIcon.png" alt="stake icon" />
               <text>
               <p>The Algo Leagues NFTs can be staked on the platform, allowing you to earn rewards in the form of ALC (Algo Leagues Coin). By staking your NFTs, you're also contributing to the security and decentralization of the Algo Leagues.</p>
               </text>
@@ -60,7 +60,7 @@ const InfoTab = () => {
               <Stuff>
               <span> 
               <h1>Battle</h1>
-              <img src="images/icons/battleIcon.png" alt=" battle icon" />
+              <img src="images/icons/battleIcon.png" alt="battle icon" />
               <text>
               <p>Algo Leagues also features a unique battle simulation game called Block Wars. Battle your Algo Leagues characters against other players' characters for a chance to win rewards in ALC and other partner tokens. You can also equip your characters with Catalyst NFTs, which provide a 33% boost to attack, defense, or health during battles.</p>
               </text>
@@ -79,7 +79,7 @@ const InfoTab = () => {
               <Stuff>
               <span> 
               <h1>Upgrade</h1>
-              <img src="images/icons/upgradeIcon.png" alt=" upgrade icon" />
+              <img src="images/icons/upgradeIcon.png" alt="upgrade icon" />
               <text>
               <p>Want to boost the power of your Algo Leagues characters? You can upgrade them by utilizing 3 catalysts (1 of each), create a higher rarity NFT. For example, 3 Catalysts earns a rare, a rare and 3 catalysts earns an Epic, and 3 catalysts and Epic earns a Legend.</p>
               </text>
@@ -98,7 +98,7 @@ const InfoTab = () => {
               <Stuff>
               <span> 
               <h1>Trade</h1>
-              <img src="images/icons/tradeIcon.png" alt=" trade icon" />
+              <img src="images/icons/tradeIcon.png" alt="trade icon" />
               <text>
               <p>Algo Leagues NFTs can be bought and sold on the Algorand blockchain through various marketplaces like Algogems.io. Trade your NFTs with other collectors to complete your collection or to upgrade your characters.</p>              </text>
               </span> 
@@ -116,7 +116,7 @@ const InfoTab = () => {
               <Stuff>
               <span> 
               <h1>Wear</h1>
-              <img src="images/icons/wearIcon.png" alt=" wear icon" />
+              <img src="images/icons/wearIcon.png" alt="wear icon" />
               <text>
               <p>Algo Leagues characters aren't just limited to the virtual world, you can also showcase your NFTs in the physical world with Algo Leagues merchandise. Show off your favorite characters with Algo Leagues apparel, stickers, and more.</p>              
               </text>
