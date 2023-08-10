9f7df4d506f63f67c10eec6ec471717e79e7a933
Zerodesignz <Japzdesignz@google.com>
2023-08-10 18:06:11 -0400

    Updates

--- src/utils/algo.js
+++
@@ -1,9 +0,0 @@
-import pkg from 'algosdk';
-
-import {config} from './config';
-
-const {Algodv2, Indexer} = pkg;
-
-export const algoClient = new Algodv2('', config.ALGO_URL, '');
-
-export const algoIndexer = new Indexer('', config.INDEXER_URL, '')
